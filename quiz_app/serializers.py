cbbe975ae673e877ae6864e788fd98830e683d8a
mcstao <arislg99@gmail.com>
2024-01-10 00:49:52 +0600

    Created serializer

---
+++ quiz_app/serializers.py
@@ -0,0 +1,54 @@
+from rest_framework import serializers
+from .models import Category, Article, Quiz, Question, Answer
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'category', 'content', 'read_time', 'art_image', 'date_created', 'is_archive']
+
+
+class WelcomeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quiz
+        fields = ['id', 'quiz_image', 'title', 'welcome_page']
+
+
+class QuizSerializer(serializers.ModelSerializer):
+    question_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Quiz
+        fields = ['id', 'title', 'category', 'quiz_image', 'question_count', ]
+
+    def get_question_count(self, obj):
+        return obj.question_count()
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = [
+            'id',
+            'answer_text',
+            'is_right',
+        ]
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    answer = AnswerSerializer(many=True, read_only=True)
+    quiz = QuizSerializer(read_only=True)
+
+    class Meta:
+        model = Question
+        fields = [
+            'quiz', 'title', 'answer'
+        ]
